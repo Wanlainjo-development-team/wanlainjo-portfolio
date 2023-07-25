7d26a77798891d9750b4ac83423712d50df59c25
rukkiecodes <rukkiecodes@gmail.com>
2023-07-26 00:02:02 +0100

    done setting up home vue

---
+++ src/components/Skills.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-row>
+        <v-col cols="12">
+            <v-card color="transparent" flat>
+                <v-card-title class="mx-0 px-0 text-h4">skills()</v-card-title>
+            </v-card>
+        </v-col>
+        <v-col cols="12" class="mt-n5">
+            <span class="text-blue-grey-darken-1">// 5+ years of experience</span>
+        </v-col>
+        <v-col cols="12">
+            <span>Main skills</span>
+        </v-col>
+        <v-col cols="12" sm="3">
+            <span class="text-blue-grey-darken-1">Frontend development</span>
+            <span class="text-blue-grey-darken-1">Backend development</span>
+            <span class="text-blue-grey-darken-1">Mobile development</span>
+        </v-col>
+        <v-col cols="12" sm="9">
+            <span class="text-blue-grey-darken-1">JavaScript, CSS, TypeScript, Vue, Node, Epress, Grapgh, 3D
+                Illustrator, and Strategic thinking</span>
+        </v-col>
+    </v-row>
+</template>
---
+++ src/layouts/AppBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app-bar flat fixed class="app-bar">
+    <v-app-bar-title>
+      <v-icon icon="mdi-circle-slice-6" />
+
+      Essentials Preset
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup>
+  //
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Fira+Code&display=swap");
+
+* {
+  font-family: "Fira Code", monospace !important;
+}
+
+.app-bar {
+  background: #1212128a !important;
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+}
+</style>
